c0cb54ba296cb3510a11c0e0c2e52b92806fac32
komorebi920 <85144958+komorebi920@users.noreply.github.com>
2022-08-27 22:22:04 +0800

    add @mdi/font

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,20 @@
+import '@mdi/font/css/materialdesignicons.css'
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    icons: {
+        iconfont: 'fa' || 'md'
+    },
+    theme: {
+        themes: {
+            dark: {
+                background: '#f4f5f9'
+            }
+        }
+    }
 });
